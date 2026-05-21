72a2eb00f6db89aaaa9c44589d8761ebf3a58d49
J. Paul Campos  T. <100632406+Akicoders@users.noreply.github.com>
2026-05-20 19:18:44 -0500

    Add env configuration to frontend/astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  env: {
+    schema: {
+      PUBLIC_API_URL: new URL('').toString(),
+    }
+  }
+});
